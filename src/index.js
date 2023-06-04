e809e41915d9671e22182a1ec9d8d54af3b55ed1
Hennadii Bublikov <99074724+HennadiiBu@users.noreply.github.com>
2023-06-04 14:24:54 +0300

    start marckup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
 import './index.css';
+import UserProfile from './components/social-profile/social-profile';
+import user from './path/user.json'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div className="container">
+    <UserProfile username={user.username} tag={user.tag} location={user.location} avatar={user.avatar} stats={user.stats}/>
+  </div>
 );
